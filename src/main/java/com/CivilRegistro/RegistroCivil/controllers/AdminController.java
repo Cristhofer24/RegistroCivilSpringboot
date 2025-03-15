dd1879553b99b7381acd0422d5f1ae6e305e9f96
cristhofer24 <you@example.com>
2025-03-15 04:05:34 -0500

    Registro de dnis y ciudadanos

--- src/main/java/com/CivilRegistro/RegistroCivil/controllers/AdminController.java
+++ src/main/java/com/CivilRegistro/RegistroCivil/controllers/AdminController.java
@@ -1,15 +1,32 @@
 package com.CivilRegistro.RegistroCivil.controllers;
 
+import com.CivilRegistro.RegistroCivil.models.Ciudadano;
+import com.CivilRegistro.RegistroCivil.models.DNI;
+import com.CivilRegistro.RegistroCivil.services.CiudadanoService;
+import com.CivilRegistro.RegistroCivil.services.DNIService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    @Autowired
+    private CiudadanoService ciudadanoService;
+    @Autowired
+    private DNIService dniService;
 
     @GetMapping("/dashboard")
-    public String admin() {
+    public String admin(Model model) {
+        List<Ciudadano> ciudadanos = ciudadanoService.obtenerTodos();
+        model.addAttribute("ciudadanos", ciudadanos);
+
+        List<DNI> dnis = dniService.obtenerTodos();
+        model.addAttribute("dnis", dnis);
         return "admin/admin";
     }
 
--- src/main/java/com/CivilRegistro/RegistroCivil/controllers/DNIController.java
+++ src/main/java/com/CivilRegistro/RegistroCivil/controllers/DNIController.java
@@ -1,6 +1,8 @@
 package com.CivilRegistro.RegistroCivil.controllers;
 
+import com.CivilRegistro.RegistroCivil.models.Ciudadano;
 import com.CivilRegistro.RegistroCivil.models.DNI;
+import com.CivilRegistro.RegistroCivil.services.CiudadanoService;
 import com.CivilRegistro.RegistroCivil.services.DNIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,8 +18,13 @@ public class DNIController {
     @Autowired
     private DNIService dniService;
 
+    @Autowired
+    private CiudadanoService ciudadanoService;
+
     @GetMapping
     public String registroDNI(Model model) {
+        List<Ciudadano> ciudadanos = ciudadanoService.obtenerTodos();
+        model.addAttribute("ciudadanos", ciudadanos);
         model.addAttribute("dni", new DNI());
         return "pages/DNI";
     }
